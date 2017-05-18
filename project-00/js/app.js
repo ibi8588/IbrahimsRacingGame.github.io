cb02f885ba004991180a611eeb19f608a0b64958
ibi8588 <ed8588@gmail.com>
2017-05-17 21:34:31 -0700

    cleanup

--- project-00/js/app.js
+++ project-00/js/app.js
@@ -1,29 +1,21 @@
 console.log("test")
+window.onload = function() {
+  $('#saffi').animate({
+    left: '+=1000px'
+  });
+}
 $(document).ready(function() {
   let $tarmo = $('#tarmo');
   let $lhurgo = $('#lhurgo');
-  // $('#go').click(function() {
-  //   var saffiWidth = $('#saffi').width();
-  //   var trackWidth = $(document).width() - saffiWidth;
-  //
-  //   $('#go').removeClass('infinite');
-  //   $('img').removeClass('rollIn');
-  //   $('#saffi').addClass('infinite bounce');
-  //   $('#saffi').animate({left: trackWidth}, 4000);
-  //
-  //   $(document).keydown(function(key) {
-  //     var positionOne = $($tarmo).position();
-  //     var positionTwo = $($lhurgo).position();
-  //     if (positionOne.left + $($lhurgo).width() >= trackWidth) {
-  //       $(document).off('keydown');
-  //       $('#container').append('<img id="dinner" src="imgs/winner.png"></img>');
-  //       $('h1').text('Tarmo Wins!!!');
-  //     }
-  //     if (positionTwo.left + $($tarmo).width() >= trackWidth) {
-  //       $(document).off('keydown');
-  //       $('#container').append('<img id="dinner" src="imgs/winner.png"></img>');
-  //       ('h1').text('Lhurgo Wins!!!!');
-  //     }
+  let maxWidth = $(window).width();
+  function winner() {
+    if($tarmo.width ==  100) {
+      $('#container').append('<img id="dinner" src="imgs/winner.jpg"></img>');
+    }
+    if($lhurgo.css('style') == 'left: 1000px') {
+      $('#container').append('<img id="dinner" src="imgs/winner.jpg"></img>');
+    }
+  }
   $(document).keypress(function(e) {
     if (e.key === "a") {
       $('#tarmo').animate({
@@ -31,27 +23,12 @@ $(document).ready(function() {
       });
     } else if (e.key === "l") {
       $('#lhurgo').animate({
-         left: '+=25px'
-       });
+        left: '+=25px'
+      });
     } else if (e.key === "h") {
-       $('#saffi').animate({
-         left: '+=25px'
-       });
-     }
- });
-  //     switch(key.which) {
-  //       //65 = a
-  //       case 65:
-  //         $tarmo.css('left', positionOne.left + 40 + 'px');
-  //         break;
-  //         //76 = l
-  //       case 76:
-  //         $lhurgo.css('left', positionOne.left + 40 + 'px');
-  //         break;
-  //     }
-  //
-  //   });
-  //   $('#reset').click(reset);
-  // });
-
+      $('#saffi').animate({
+        left: '+=25px'
+      });
+    }
+  });
 });
